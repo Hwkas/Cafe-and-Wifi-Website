df2c7e7d8acd15ccb8187f41c7ac01a52a6df224
amaan_saifi <amaansaifi999@gmail.com>
2023-01-18 21:45:31 +0530

    added templates

--- cafe/urls.py
+++ cafe/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", view=home, name="home")
+    path("", view=home, name="home"),
+    path("add-cafe", view=add_cafe, name="add-cafe"),
 ]
